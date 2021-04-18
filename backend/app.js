04a59b5b18e117eb141e6fb56bf489840bc99ee0
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2021-04-18 13:46:25 +0200

    add mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,18 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
 const port = 3000
+
+const mongoDB = 'mongodb://127.0.0.1/my_database';
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+
+//Get the default connection
+const db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
