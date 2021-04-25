75f3f8fd795ed7ba27ccf89e67f15d2b0b912d95
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2021-04-25 16:16:49 +0200

    Add CORS

--- backend/app.js
+++ backend/app.js
@@ -2,15 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 
 const port = 3000
+
+const corsOptions = {
+    origin: 'http://localhost:5000',
+    optionsSuccessStatus: 200 // For legacy browser support
+}
 
 const mongoDB = 'mongodb://127.0.0.1/my_database';
 mongoose
     .connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => {
         const app = express();
-        app.use(bodyParser.json())
+        app.use(cors(corsOptions));
+        app.use(bodyParser.json());
 
         app.use('/api/v0', routes);
 
