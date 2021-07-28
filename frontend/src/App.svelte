b0f8a1cabfcafb0066e49bc8ae0bd177863d523c
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2021-07-28 20:19:24 +0200

    move navigation bar to separate component

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,47 +1,12 @@
 <script>
-    import { Router, Route, Link } from 'svelte-navigator';
+    import {Router, Route} from 'svelte-navigator';
     import AdmissionsPage from "./pages/Admissions/AdmissionsPage.svelte";
     import AdmissionPage from "./pages/Admission/AdmissionPage.svelte";
 </script>
 
-<style>
-    nav {
-        /*background-color: #0095DD;*/
-        background: #666666;
-        height: 60px;
-        display: flex;
-    }
-    nav .logo {
-        margin: auto 20px auto 10px;
-        height: 100%;
-        display: flex;
-        align-items: center;
-    }
-    nav .logo>span {
-        font-size: 1.3em;
-    }
-    nav .logo>img {
-        height: 80%;
-    }
-    nav :global(.navLink) {
-        display: inline-block;
-        line-height: 60px;
-        padding: 0 20px;
-        background: #0072BC;
-        color: white;
-    }
-</style>
 
 <Router>
-    <header>
-        <nav>
-            <div class="logo">
-                <img src="/shift-logo-with-text.png">
-                <span>Admissions System</span>
-            </div>
-            <Link class="navLink" to="/">Home</Link>
-        </nav>
-    </header>
+    <NavigationBar />
     <main>
         <Route component={AdmissionsPage} />
         <Route path="/admission/*admissionId" component={AdmissionPage} />
---
+++ frontend/src/components/NavigationBar.svelte
@@ -0,0 +1,41 @@
+<script>
+    import {Link} from 'svelte-navigator';
+</script>
+
+<style>
+    nav {
+        /*background-color: #0095DD;*/
+        background: #666666;
+        height: 60px;
+        display: flex;
+    }
+    nav .logo {
+        margin: auto 20px auto 10px;
+        height: 100%;
+        display: flex;
+        align-items: center;
+    }
+    nav .logo>span {
+        font-size: 1.3em;
+    }
+    nav .logo>img {
+        height: 80%;
+    }
+    nav :global(.navLink) {
+        display: inline-block;
+        line-height: 60px;
+        padding: 0 20px;
+        background: #0072BC;
+        color: white;
+    }
+</style>
+
+<header>
+    <nav>
+        <div class="logo">
+            <img src="/shift-logo-with-text.png">
+            <span>Admissions System</span>
+        </div>
+        <Link class="navLink" to="/">Home</Link>
+    </nav>
+</header>
