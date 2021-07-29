72cab149bbb8517a5d9502a1a2a4ef40eb18c6e6
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2021-07-29 11:15:49 +0200

    Backend require admission name

--- backend/models/Admission.js
+++ backend/models/Admission.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({
-    name: String,
+    name: { type: String, required: true },
     date: { type: Date, default: Date.now() },
 });
 
