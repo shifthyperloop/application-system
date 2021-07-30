b86aa06ea3fce1b6287f1548b6a0d63892de7b35
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2021-07-30 15:32:27 +0200

    Add group schema

---
+++ backend/models/Group.ts
@@ -0,0 +1,21 @@
+import {Schema, Document, model} from "mongoose";
+
+export interface IGroup extends Document {
+  admissionId: string,
+  name: string,
+}
+
+const schema = new Schema<IGroup>({
+  admissionId: {
+    required: true,
+    type: Schema.Types.ObjectId,
+  },
+  name: {
+    required: true,
+    type: String,
+  }
+});
+
+const Group = model<IGroup>("Group", schema);
+
+module.exports = Group;
