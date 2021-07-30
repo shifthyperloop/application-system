dac7e3b5d9ead915b98ae8a369aa61496b0907cb
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2021-07-30 15:53:18 +0200

    add necessary fields to Admission schema

--- backend/models/Admission.ts
+++ backend/models/Admission.ts
@@ -1,17 +1,40 @@
-import mongoose from "mongoose";
+import mongoose, {Schema} from "mongoose";
 
 export interface IAdmission extends mongoose.Document {
     name: string;
     date: Date;
+    deleteData: Date;
+    userIds: string[];
+    admittingLeaders: boolean;
+    admittingMembers: boolean;
 }
 
-const schema = new mongoose.Schema<IAdmission>({
+const schema = new Schema<IAdmission>({
     name: {
         type: String,
         required: true,
         unique: "Admission with this name already exists",
     },
-    date: { type: Date, default: Date.now() },
+    date: {
+        type: Date,
+        default: Date.now()
+    },
+    deleteDate: {
+        required: true,
+        type: Date,
+    },
+    userIds: {
+        required: true,
+        type: [Schema.Types.ObjectId],
+    },
+    admittingLeaders: {
+        required: true,
+        type: Boolean,
+    },
+    admittingMembers: {
+        required: true,
+        type: Boolean,
+    }
 });
 
 export default mongoose.model<IAdmission>("Admission", schema);
